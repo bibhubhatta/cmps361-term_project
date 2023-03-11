3eeb90667f08d8b5753e86b0496e88b34aeb7fa3
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-03-11 05:42:51 +0000

    Comment out windows specific include files

--- stdafx.h
+++ stdafx.h
@@ -7,7 +7,7 @@
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 #include <stdio.h>
-#include <tchar.h>
+// #include <tchar.h>
 
 // TODO: reference additional headers your program requires here
 // Standard include files.
@@ -15,7 +15,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
-#include <windows.h>
+// #include <windows.h>
 #include <map>
 #include <vector>
 
