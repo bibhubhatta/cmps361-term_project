7c20c97fb9e2f4f5b85eebe84f86d39a390c8e04
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-03-21 12:46:59 -0400

    Enable googletest

---
+++ tests/test_test.cpp
@@ -0,0 +1,11 @@
+// Test Google Test
+
+#include <assembler_lib/Assembler.h>
+#include <gtest/gtest.h>
+
+TEST(TestTest, TestTest) { EXPECT_EQ(1, 1); }
+
+TEST(AssemblerTest, AssemblerTest)
+{
+    Assembler assem("D:\\Projects\\cmps361-term_project\\factorial.txt");
+}
