529b45bf58927cb8aca952bb00ec8e06c1490c4a
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-04-09 00:22:00 -0400

    Refactor

--- scripts/rename_variable_names.py
+++ scripts/rename_variable_names.py
@@ -8,15 +8,24 @@ def is_cpp_file(file_name):
     return file_name.endswith(".cpp") or file_name.endswith(".h")
 
 
-for file in os.listdir(path):
+def prefix_class_members(data):
+    # Replace all variable names starting with "_" with "m_"
+    data = re.sub(r'\b_[a-zA-Z0-9_]*\b', lambda x: "m" + x.group(0), data)
+    return data
 
-    if is_cpp_file(file):
+
+def main():
+    cpp_files = [file for file in os.listdir(path) if is_cpp_file(file)]
+    for file in cpp_files:
         with open(path + file, "r") as f:
             data = f.read()
 
-        # Replace all variable names starting with "_" with "m_"
-        data = re.sub(r'\b_[a-zA-Z0-9_]*\b', lambda x: "m" + x.group(0), data)
+        data = prefix_class_members(data)
 
         with open(path + file, "w") as f:
             # Write the file
             f.write(data)
+
+
+if __name__ == "__main__":
+    main()
